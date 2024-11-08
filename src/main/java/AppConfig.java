3cbea29cce1d30de43498ca6b77d39b32fe7f9ca
Chief <cheefirqa@gmail.com>
2024-11-08 19:30:50 +0300

    задача выполнена

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat cat() {
+        return new Cat();
+    }
  
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
